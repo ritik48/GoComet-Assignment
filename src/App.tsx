56553c23372cbc8bd05a88370f729b22a1fc67f3
Ritik <raj76941@gmail.com>
2025-01-18 11:47:17 +0530

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Pages/Home/Home";
+import { Hotel } from "./Pages/Hotel/Hotel";
+import NavBar from "./components/NavBar/NavBar";
+import "./index.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route index path="/hotels" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
