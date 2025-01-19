ea9d1094e8c212c6a9433c1acc85452dcb59c8e4
Ritik <raj76941@gmail.com>
2025-01-19 22:05:40 +0530

    feat: added hotels in the explore section

--- src/Pages/Home/components/Explore.tsx
+++ src/Pages/Home/components/Explore.tsx
@@ -1,22 +1,27 @@
 import { Filter } from "../../../components/Filter/Filter";
 import style from "./Explore.module.css";
+import { HotelList } from "./HotelList";
 
 export function Explore() {
   return (
     <div className={style.explore}>
       <div className={`${style.top}`}>
         <h3>Explore Hotels</h3>
-        <div className={style.bottom}>
+        <div className={`${style.bottom} flex`}>
           <Filter />
-          <div className={style.right}>
-            <select defaultValue={2} className={style.right_top}>
+          <div className={`${style.right} flex flex-col`}>
+            <select className={`${style.sort}`}>
               {/* <option value="none">Total People</option> */}
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
+              <option value="none" selected>
+                Sort By
+              </option>
+              <option value="a-z">Name: A - Z</option>
+              <option value="z-a">Name: Z - A</option>
+              <option value="h-l">Price: Hight to Low</option>
+              <option value="l-h">Price: Low to High</option>
             </select>
             <div className={style.right_bottom}>
-              <div></div>
+              <HotelList />
             </div>
           </div>
         </div>
