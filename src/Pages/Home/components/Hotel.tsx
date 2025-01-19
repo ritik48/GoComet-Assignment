cdf78143f0f87c7ede6238cd319ab96ed653d3cd
Ritik <raj76941@gmail.com>
2025-01-19 23:32:23 +0530

    feat: added component and style for individual hotel

--- src/Pages/Home/components/Hotel.tsx
+++ src/Pages/Home/components/Hotel.tsx
@@ -1,5 +1,8 @@
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { FaStar } from "react-icons/fa";
+
 import style from "./Hotel.module.css";
+import { Link } from "react-router-dom";
 
 type HotelType = {
   name: string;
@@ -21,8 +24,11 @@ export function Hotel({ data }: { data: HotelType }) {
       <div className={`flex flex-col ${style.top}`}>
         <div className="flex items-center">
           <div className={style.hotel_name}>{data.name}</div>
-          <div className={style.rating_container}>
-            <div className={style.hotel_rating}>{data.rating}</div>
+          <div className={`${style.rating_container} flex items-center`}>
+            <FaStar size={15} />
+            <div className={style.hotel_rating}>
+              {parseFloat(data.rating).toFixed(1)}
+            </div>
           </div>
         </div>
         <div className={style.hotel_city}>{data.city}</div>
@@ -31,9 +37,11 @@ export function Hotel({ data }: { data: HotelType }) {
         <div className={style.price}>
           {data.rooms[0].price} - {data.rooms[data.rooms.length - 1].price}
         </div>
-        <button className="btn primary flex items-center">
-          View <IoIosArrowRoundForward size={15} />
-        </button>
+        <Link to={`/hotel/${data.id}`}>
+          <button className={`btn primary flex items-center ${style.view}`}>
+            View <IoIosArrowRoundForward size={18} />
+          </button>
+        </Link>
       </div>
     </div>
   );
