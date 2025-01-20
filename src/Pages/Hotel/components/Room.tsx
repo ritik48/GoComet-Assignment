3ed7b9a67734e23449dd9ff3ce443598643cf907
Ritik <raj76941@gmail.com>
2025-01-20 16:16:03 +0530

    feat: added component to display individual rooms

---
+++ src/Pages/Hotel/components/Room.tsx
@@ -0,0 +1,45 @@
+import style from "./HotelRooms.module.css";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { MdPeopleAlt } from "react-icons/md";
+import { RoomType } from "./DisplayRooms";
+import { useState } from "react";
+import Modal from "../../../components/modal";
+import { Booking } from "./Booking";
+
+export function Room({ data }: { data: RoomType }) {
+  const [openBookingModal, setOpenBookingModal] = useState(false);
+
+  return (
+    <>
+      <div className={style.hotel_container}>
+        <img src={data.image_urls[0]} className={style.image} />
+        <div className={`flex flex-col ${style.content}`}>
+          <div className={`flex items-center ${style.content_top}`}>
+            <div className={style.room_name}>{data.name}</div>
+            <div className={`${style.rating_container} flex items-center`}>
+              <MdPeopleAlt size={18} />
+              <div className={style.total_people}>2</div>
+            </div>
+          </div>
+          <div className={style.price_content}>
+            <span className={style.price}>{data.price}</span> / night
+          </div>
+          <div className={`flex items-center ${style.action_container}`}>
+            <button className={`btn secondary flex items-center`}>
+              View Facilities
+            </button>
+            <button
+              onClick={() => setOpenBookingModal(true)}
+              className={`btn primary flex items-center ${style.view}`}
+            >
+              Book Now <IoIosArrowRoundForward size={18} />
+            </button>
+          </div>
+        </div>
+      </div>
+      <Modal isOpen={openBookingModal}>
+        <Booking room={data} onClose={() => setOpenBookingModal(false)} />
+      </Modal>
+    </>
+  );
+}
